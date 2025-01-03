7300690946659086daab52058676d62d9116bc5c
harsifatsingh <harsifat05@gmail.com>
2025-01-03 15:44:56 -0500

    Implemented the bishop piece

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -1,4 +1,5 @@
 from .piece import Piece
+from move import Move
 
 class Bishop(Piece):
     def __init__(self, color):
@@ -9,4 +10,25 @@ class Bishop(Piece):
         Return a list of possible moves for a bishop.
         Moves are any number of squares diagonally until blocked.
         """
-        pass
+        moves = []
+        directions = [(1,1), (-1,1), (1,-1), (-1,-1)]
+
+        for drow, dcol in directions:
+            r, c = row, col
+            while True:
+                r += drow
+                c += dcol
+                if 0 <= r < 8 and 0 <= c < 8:
+                    target_piece = board.get_piece_at(r, c)
+                    if target_piece is None:
+                        moves.append(Move(row, col, r, c, board.get_piece_at(row, col)))  
+                    else:
+                        if target_piece.color != self.color:
+                            moves.append(Move(row, col, r, c, board.get_piece_at(row, col), target_piece))
+                        break
+                else:
+                    break
+        return moves
+    
+    def __str__(self):
+        return "B" if self.color == "white" else "b"
