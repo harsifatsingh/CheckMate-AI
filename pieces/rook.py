d14b8880116879413beafaa231bd064b75645eb8
harsifatsingh <harsifat05@gmail.com>
2025-01-03 15:38:37 -0500

    Implement the rook piece

--- pieces/rook.py
+++ pieces/rook.py
@@ -1,4 +1,5 @@
 from .piece import Piece
+from move import Move
 
 class Rook(Piece):
     def __init__(self, color):
@@ -9,4 +10,25 @@ class Rook(Piece):
         Return a list of possible moves for a rook.
         Moves are any number of squares along rank/file until blocked.
         """
-        pass
+        moves = []
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        for drow, dcol in directions:
+            r, c = row, col
+            while True:
+                r += drow
+                c += dcol
+                if 0 <= r < 8 and 0 <= c < 8:
+                    target_piece = board.get_piece_at(r, c)
+                    if target_piece is None:
+                        moves.append(Move(row, col, r, c, board.get_piece_at(row, col)))  
+                    else:
+                        if target_piece.color != self.color:
+                            moves.append(Move(row, col, r, c, board.get_piece_at(row, col), target_piece))
+                        break
+                else:
+                    break
+        return moves  
+    
+    def __str___(self):
+        return "R" if self.color == "white" else "r"
