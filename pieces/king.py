b1045e7e408329a4e4a4d8c59e5d9e1a8ea68a7f
harsifatsingh <harsifat05@gmail.com>
2025-01-03 15:50:56 -0500

    Implement King class

--- pieces/king.py
+++ pieces/king.py
@@ -1,13 +1,30 @@
 from .piece import Piece
+from move import Move
 
 class King(Piece):
     def __init__(self, color):
         super().__init__(color)
 
     def get_legal_moves(self, board, row, col):
-        """
-        Return a list of possible moves for a king.
-        Moves one square in any direction if it’s not in check.
-        Also handle castling if allowed.
-        """
-        pass
+        moves = []
+        king_moves = [
+            (row+1, col),   (row-1, col),
+            (row, col+1),   (row, col-1),
+            (row+1, col+1), (row+1, col-1),
+            (row-1, col+1), (row-1, col-1)
+        ]
+        for (r, c) in king_moves:
+            if 0 <= r < 8 and 0 <= c < 8:
+                target_piece = board.get_piece_at(r, c)
+                if target_piece is None:
+                    moves.append(Move(row, col, r, c, board.get_piece_at(row, col)))
+                else:
+                    # capture if enemy
+                    if target_piece.color != self.color:
+                        moves.append(Move(row, col, r, c, board.get_piece_at(row, col), target_piece))
+
+        # In a full implementation, you'd handle castling here.
+        return moves
+
+    def __str__(self):
+        return 'K' if self.color == "WHITE" else 'k'
